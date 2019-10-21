3c26ab2e5584aeab60b144a14975ecf316feb48c
eche <spwesn@gmail.com>
2019-10-21 01:01:25 +0100

    update mobile.vue

--- src/components/agenda/mobile.vue
+++ src/components/agenda/mobile.vue
@@ -2,14 +2,14 @@
 <v-container class="pa-0 my-0">
         <v-layout  class="my-0 mt-2" >
             <v-flex  class="pa-2 my-0" >
-        <div style="border-radius: 5px; border:1px solid purple;min-height:180px" class="ma-1 pa-5" v-for="person in mobile" :key="person.id">
+        <div style="border-radius: 5px; border:1px solid purple;min-height:180px" class="ma-1 pa-5" v-for="person in schedule2019" :key="person.id">
               <v-layout row wrap justify-center align-self-center>
                          <v-flex xs12 sm2 md2 >
                            <v-responsive>
                             <v-avatar color="grey lighten-2 my-auto" size="110" tile="tile">
                               <v-img
-                               :src ="person.profileImg"
-                                :lazy-src="person.profileImg"
+                               :src ="person.sessionImage"
+                                :lazy-src="person.sessionImage"
                                
                                >
                             <v-layout
@@ -27,13 +27,13 @@
                        </v-responsive>
                          </v-flex>
                          <v-flex xs12  sm6 md7 class="">
-                             <div class="google-font"  style="font-size:100%;color: rgb(13, 71, 161);">{{person.desc}}</div>
-                             <div class="google-font text--light-blue lighten-1" style="font-size:110%; text-transform: capitalize;"> by {{person.name}}</div>
-                            <div class="caption grey--text google-font" > {{person.role}} </div>
+                             <div class="google-font"  style="font-size:100%;color: rgb(13, 71, 161);">{{person.sessionDesc}}</div>
+                             <div class="google-font text--light-blue lighten-1" style="font-size:110%; text-transform: capitalize;"> by {{person.speakerName}}</div>
+                            <div class="caption grey--text google-font" > {{person.sessionTitle}} </div>
                          </v-flex>
                          <v-flex xs12 sm2 md2>
-                            <v-chip color="green" class="ma-1 left" outline small>talk time: {{person.time}}</v-chip>
-                            <v-chip color="red" class="ma-1 left" outline small> duration:  {{person.duration}} </v-chip>
+                            <v-chip color="green" class="ma-1 left" outline small>talk time: {{person.sessionStartTime}}</v-chip>
+                            <v-chip color="red" class="ma-1 left" outline small> duration:  {{person.sessionTotalTime}} </v-chip>
                          </v-flex>                   
               </v-layout>  
                 </div>   
@@ -47,16 +47,17 @@ import db from '@/fb'
 export default {
     data() {
        return {
-           mobile:[]
+           schedule2019:[]
        }
     },
   created () {
-    db.collection('mobile').orderBy("time", "desc").onSnapshot(res => {
+    db.collection("schedule2019").where("track", "==",'mobile')
+    //  .orderBy("", "desc")
+    .onSnapshot(res => {
       const changes = res.docChanges();
-
       changes.forEach(change => {
         if (change.type === 'added'){
-          this.mobile.push({
+          this.schedule2019.push({
             ...change.doc.data(),
             id: change.doc.id
           })
