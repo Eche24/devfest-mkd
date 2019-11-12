ee36939e490d3e0a3cc441fdd9cdc36d20736edd
eche ngbede <spwesn@gmail.com>
2019-11-12 21:11:49 +0100

    Update fb.js

--- src/fb.js
+++ src/fb.js
@@ -4,13 +4,7 @@ import 'firebase/firestore'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyChi-hyIfzpvWg0ehU18K5dS_Pz5dSrTds",
-  authDomain: "devfestmakurdi.firebaseapp.com",
-  databaseURL: "https://devfestmakurdi.firebaseio.com",
-  projectId: "devfestmakurdi",
-  storageBucket: "devfestmakurdi.appspot.com",
-  messagingSenderId: "1021366833604",
-  appId: "1:1021366833604:web:f71bd2a311b7a144"
+ // firebase config go here
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
